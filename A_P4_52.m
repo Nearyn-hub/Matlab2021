7fee5b863281ce676d42bdef9a87d4f2b7ed5863
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-13 15:50:01 +0100

    Add files via upload

---
+++ A_P4_52.m
@@ -0,0 +1,14 @@
+% plot function
+f = @(x) (2*x^2 + 3*x) / (x^2 + 4*x + 5);
+hold on
+fplot(f)
+plot(-2, f(-2), 'r*') % origin
+hold off
+
+% style plot
+grid on
+axis([-10 10 -4 8])
+legend("(2*x^2 + 3*x) / (x^2 + 4*x + 5)", "origo");
+title("Block A P4.52")
+xlabel("x-axis")
+ylabel("y-axis")
